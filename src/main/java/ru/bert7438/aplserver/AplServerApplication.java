dea5028f53f6b2e37dfa207ec58061cc58cdace3
bert7438 <bert7438@gmail.com>
2022-01-14 18:41:49 +0300

    Initial commit

---
+++ src/main/java/ru/bert7438/aplserver/AplServerApplication.java
@@ -0,0 +1,13 @@
+package ru.bert7438.aplserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AplServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/bert7438/aplserver/AplServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.bert7438.aplserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
