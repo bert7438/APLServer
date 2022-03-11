276541947495d56d90daa44f827f1952ff118b86
bert7438 <bert7438@gmail.com>
2022-03-11 15:53:42 +0300

    added Teacher stub

---
+++ src/main/java/ru/bert7438/aplserver/entities/Teacher.java
@@ -0,0 +1,13 @@
+package ru.bert7438.aplserver.entities;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Data
+@Entity
+@Table(name="teachers")
+public class Teacher {
+//TODO: add all
+}
